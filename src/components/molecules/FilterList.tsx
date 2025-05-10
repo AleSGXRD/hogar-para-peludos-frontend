9c94281af7f98b5d54b1ac55e709a4ea35c40c94
AleSGXRD <alesgxrd@gmail.com>
2025-05-10 13:13:37 -0400

    appearence-none

--- src/components/molecules/FilterList.tsx
+++ src/components/molecules/FilterList.tsx
@@ -47,9 +47,8 @@ export default function FilterList({ list }: Props) {
             placeholder="Buscar mascota..."
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            className="bg-gray-50 transition-all shadow-md border border-gray-100 rounded-lg px-4 py-2 pl-10 focus:border-[#76ca99]
-              min-w-80 focus:outline-[#76ca99] w-full md:w-auto 
-              appearance-none"
+            className="bg-gray-50 transition-all shadow-md border border-gray-100 px-4 py-2 pl-10 focus:border-[#76ca99]
+              min-w-80 focus:outline-[#76ca99] w-full md:w-auto appearance-none rounded-lg"
           />
           <svg className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" 
             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
