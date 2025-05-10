d291df1c7ffea0a092de8da2753f4b7c63281caa
AleSGXRD <alesgxrd@gmail.com>
2025-05-10 11:35:40 -0400

    add meta

--- src/components/molecules/FilterList.tsx
+++ src/components/molecules/FilterList.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import type { Pet } from '../../lib/types/Pet'
-import AdoptionCard from '../atoms/AdoptionCard'
 import PetGrid from './PetGrid'
 import { CustomSelect, genderIcons } from '../atoms/selects/CustomSelect'
 
@@ -49,7 +48,8 @@ export default function FilterList({ list }: Props) {
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
             className="bg-gray-50 transition-all shadow-md border border-gray-100 rounded-lg px-4 py-2 pl-10 focus:border-[#76ca99]
-              min-w-80 focus:outline-[#76ca99] w-full md:w-auto"
+              min-w-80 focus:outline-[#76ca99] w-full md:w-auto 
+              appearance-none"
           />
           <svg className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" 
             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
